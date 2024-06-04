82ad3c476fc0a43b97cd8d329148f554cc80d906
Shay Abraham <shaykos@gmail.com>
2024-06-04 15:42:53 +0300

    added routes

--- Express/04-06-2024/src/main.ts
+++ Express/04-06-2024/src/main.ts
@@ -1,5 +1,9 @@
 //ייבוא טיפוסים ופונקציות
-import express, { Express, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+
+/*By importing this module, the routes and functionality defined in 'user.routes' can be used in the main Express application to
+handle user-related requests and logic. */
+import userRouter from './users/user.routes';
 
 //הגדרת הפורט
 const PORT = 5555;
@@ -7,5 +11,36 @@ const PORT = 5555;
 //יצירת השרת 
 const app = express();
 
+//הגדרות תצורה לשרת
+/* The line `app.use(express.json());` is setting up middleware in the Express application to parse
+incoming requests with JSON payloads. This middleware function parses incoming request bodies and
+makes the parsed JSON data available on the `req.body` property of the request object. This allows
+the application to easily work with JSON data sent in requests. */
+app.use(express.json());
+
+//יצירת ניתוב
+app.get('/', async (req: Request, res: Response) => {
+    res.status(200).send('KUKU from server!');
+});
+
+//שימוש בנתיבים של היוזרים
+//URL הפרמטר הראשון הוא הבסיס לכתובת ה 
+//עצמו Router הפרמטר השני הוא ה 
+app.use('/api/users', userRouter);
+
+// app.get('/hello', async (req: Request, res: Response) => {
+//     res.status(200).send('Hello');
+// });
+
+// app.post('/add', async (req: Request, res: Response) => {
+//     console.log(req.body)
+//     if(!req.body.name)
+//         res.status(500).send('no valid data!');
+//     else
+//         res.status(201).send('added succesfuly');
+// });
+
 //הפעלת השרת
 app.listen(PORT, () => console.log(`[Server] server running at http://localhost:${PORT}`));
+
+
---
+++ Express/04-06-2024/src/users/user.routes.ts
@@ -0,0 +1,29 @@
+
+import { Request, Response, Router } from 'express';
+
+const userRouter = Router();
+
+const users = [
+    { first_name: 'momik', last_name: 'epsilon' },
+    { first_name: 'zozik', last_name: 'barsum' }
+];
+
+userRouter.get('/', async (req: Request, res: Response) => {
+    res.status(200).json(users);
+});
+
+userRouter.post('/add', async (req: Request, res: Response) => {
+    try {
+        let u = req.body;
+        users.push(u);
+        res.status(201).json(users);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+export default userRouter;
+
+
+
+
